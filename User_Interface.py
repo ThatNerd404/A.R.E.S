5c55724fcbbec8071eedfd593df622d2ba4dfe60
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2025-07-01 21:14:03 -0500

    making basic template junk

---
+++ User_Interface.py
@@ -0,0 +1,58 @@
+from PySide6.QtGui import QMovie, QKeyEvent, QFontDatabase, QKeySequence, QShortcut, QTextCursor
+from PySide6.QtWidgets import QMainWindow, QInputDialog, QFileDialog
+from PySide6.QtCore import Qt, QPoint, QSettings, QEventLoop
+from ARES_GUI import Ui_MainWindow
+import logging
+import requests
+class UserInterface(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowTitle("A.R.E.S. - Artist Recommendation and Exploration System")
+        
+        
+        self.api_key = "6434733d6b68ed2572abb91ab1966564"
+        self.BASE_URL = 'https://ws.audioscrobbler.com/2.0/'
+    
+    def get_artist_from_track(self,track_name):
+        params = {
+            'method': 'track.search',
+            'track': track_name,
+            'api_key': self.api_key,
+            'format': 'json',
+            'limit': 1
+        }
+        response = requests.get(self.BASE_URL, params=params)
+        data = response.json()
+        
+        try:
+            track = data['results']['trackmatches']['track'][0]
+            return track['artist']
+        except (KeyError, IndexError):
+            return None, None
+        
+    def get_similar_tracks(self,track_name, artist_name,  limit=5): # 
+        params = {
+            'method': 'track.getsimilar',
+            'track': track_name,
+            'artist': artist_name,
+            'api_key': self.api_key,
+            'format': 'json',
+            'limit': limit
+        }
+        response = requests.get(self.BASE_URL, params=params)
+        data = response.json()
+
+        if 'similartracks' not in data or 'track' not in data['similartracks']:
+            return []
+
+        tracks = data['similartracks']['track']
+        return [f"{track['name']} by {track['artist']['name']}" for track in tracks]
+
+        # Try it
+    '''song_name = input("Enter a song name to find recommendations: ")
+    artist_name = get_artist_from_track(song_name)
+    similar_tracks = get_similar_tracks(song_name, artist_name)
+    print(f"Tracks similar to {song_name} by {artist_name}:")
+    for t in similar_tracks:
+        print("-", t)'''
